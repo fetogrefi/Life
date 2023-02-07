b9e4c7b5d983ef770598f8b43aae64c30641390e
fetogrefi <123547399+fetogrefi@users.noreply.github.com>
2023-02-07 16:10:58 +1100

    add

---
+++ France Landscape Map/layers/layers.js
@@ -0,0 +1,29 @@
+var wms_layers = [];
+
+
+        var lyr_France_0 = new ol.layer.Tile({
+            'title': 'France',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://fetogrefi.github.io/Life/Landscape/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_WorldBase_1 = new ol.layer.Tile({
+            'title': 'World Base',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://fetogrefi.github.io/Life/Worldbase/{z}/{x}/{y}.png'
+            })
+        });
+
+lyr_France_0.setVisible(true);lyr_WorldBase_1.setVisible(true);
+var layersList = [lyr_France_0,lyr_WorldBase_1];
